5a6ac097d3eb2300163c6b3a7e44d09ab341fb15
Andres Alvarez <ana260@pitt.edu>
2023-03-24 10:15:01 -0400

    Updated changes to "news.php" file
    1. using CDC rss feed (.rss) file to test reading as xml
    2. Using variables to control number of loaded items

--- news.php
+++ news.php
@@ -12,17 +12,38 @@
         padding: 0;
         box-sizing: border-box;
     }
-
+    .feed {
+    position: relative;
+    height: 400px;
+    overflow-y: scroll;
+    /* box-shadow: inset 0 0 20px 0 #842121; */
+    width: 20%;
+}
     .published_item {
         display: grid;
+        margin:20px;
+        padding:20px;
+        border-top:1px solid #efefef;
+        border-bottom:1px solid red;
+       
     }
+
+.feed::after {
+    content: '';
+    position: sticky;
+    width: 100%;
+    height: 50px;
+    background: linear-gradient(0deg, #ffffff,transparent,transparent);
+    display: block;
+    bottom: 0;
+}
     </style>
 </head>
 
 <body>
     <div class="feed"> <?php
                         // Load the RSS feed
-                        $rss = simplexml_load_file('./news.xml');
+                        $rss = simplexml_load_file('https://tools.cdc.gov/api/v2/resources/media/342776.rss');
 
                         // Get the channel information
                         $channel_title = $rss->channel->title;
@@ -30,17 +51,25 @@
                         $channel_description = $rss->channel->description;
 
                         // Loop through the items in the feed
-                        foreach ($rss->channel->item as $item) {
-                            // Get the item information
-                            $item_title = $item->title;
-                            $item_link = $item->link;
-                            $item_description = $item->description;
-                            $item_pubDate = $item->pubDate;
 
-                            // Do something with the item information (e.g. display it on a webpage)
-                            echo '<div class="published_item" <h4><a href="' . $item_link . '" target="_blank">' . $item_title . '</a></h4>';
-                            echo '<p>' . $item_description . '</p>';
-                            echo '<p>Published on: ' . $item_pubDate . '</p></div>';
+                        $max = 10;
+                        $i = 0;
+                        foreach ($rss->channel->item as $item) {
+
+                            if($i <= $max){
+
+                                // Get the item information
+                                $item_title = $item->title;
+                                $item_link = $item->link;
+                                $item_description = $item->description;
+                                $item_pubDate = $item->pubDate;
+                                
+                                // Do something with the item information (e.g. display it on a webpage)
+                                echo '<div class="published_item" <h4><a href="' . $item_link . '" target="_blank">' . $item_title . '</a></h4>';
+                                echo '<p>' . $item_description . '</p>';
+                                echo '<p>Published on: ' . $item_pubDate . '</p></div>';
+                                $i++;
+                            }
                         }
                         ?> </div>
 </body>
